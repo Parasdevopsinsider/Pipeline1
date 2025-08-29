b058548d19721aceb97f9d906f686b2a8a304cfc
Parasdevopsinsider <paras.tayde11@gmail.com>
2025-08-29 18:14:26 +0530

    new stg add

--- main.tf
+++ main.tf
@@ -7,3 +7,11 @@ resource "azurerm_resource_group" "rg1" {
   name     = "rg_prod"
   location = "eastus"
 }
+
+resource "azurerm_storage_account" "stgpreprod" {
+  name                     = "stpreprod"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS"
+}
